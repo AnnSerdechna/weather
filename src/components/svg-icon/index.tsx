ecc907c7765fbe4d8dcd5168a8bf20395f3964f4
Анна Сердечна <annaserdecna@MacBook-Air-Anna.local>
2024-12-16 12:54:46 +0200

    fix the svg import

---
+++ src/components/svg-icon/index.tsx
@@ -0,0 +1,38 @@
+import { FC, SVGProps } from 'react'
+
+import {
+   Celsius,
+   Fahrenheit,
+   Sun,
+   Cloud,
+   Rain,
+   Snow,
+   CloudSnowRain,
+   CloudSunSnowRain,
+   CloudWindy,
+   CloudSunSWind,
+   CloudPartly,
+} from '../../assets/icons'
+
+
+const icons = {
+   celsius: Celsius,
+   fahrenheit: Fahrenheit,
+   cloudy: Cloud,
+   rainy: Rain,
+   snowy: Snow,
+   sunny: Sun,
+   ['cloudy-snowy-rainy']: CloudSnowRain,
+   ['cloudy-sunny-snowy-rainy']: CloudSunSnowRain,
+   ['cloudy-windy']: CloudWindy,
+   ['cloudy-sunny-windy']: CloudSunSWind,
+   ['cloudy-partly']: CloudPartly,
+}
+
+type SvgIconProps = SVGProps<SVGSVGElement> & { type: keyof typeof icons }
+
+export const SvgIcon: FC<SvgIconProps> = ({ type, ...svgProps }) => {
+   const Icon = icons[type] ?? null
+
+   return Icon && <Icon {...svgProps} />
+}
